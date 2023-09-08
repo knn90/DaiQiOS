908247a13e30ac038a7a18dc9e8170d4b9d9d31b
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-09-08 11:10:10 +0700

    Update deployment target to 16.0

---
+++ DaiQiOS/DaiQiOS/Todaychecklist.swift
@@ -0,0 +1,20 @@
+//
+//  Todaychecklist.swift
+//  DaiQiOS
+//
+//  Created by Đinh Trần Việt Anh on 08/09/2023.
+//
+
+import SwiftUI
+
+struct Todaychecklist: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Todaychecklist_Previews: PreviewProvider {
+    static var previews: some View {
+        Todaychecklist()
+    }
+}
