db84f839f037a7e65ed29cda141a171378ab5030
Khoi Nguyen <khoinguyen@192.168.2.2>
2023-09-08 10:27:32 +0700

    Initial DaiQiOS project

---
+++ DaiQiOS/DaiQiOS/DaiQiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaiQiOSApp.swift
+//  DaiQiOS
+//
+//  Created by Khoi Nguyen on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct DaiQiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
