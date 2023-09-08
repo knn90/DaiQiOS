2cb1366791254e180ba8cdf0b37633998d7fb6ea
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-09-08 11:33:26 +0700

    create simple list view

--- DaiQiOS/DaiQiOS/Todaychecklist.swift
+++ DaiQiOS/DaiQiOS/Todaychecklist.swift
@@ -1,15 +1,39 @@
-//
-//  Todaychecklist.swift
-//  DaiQiOS
-//
-//  Created by Đinh Trần Việt Anh on 08/09/2023.
-//
-
 import SwiftUI
 
+struct ChecklistItem {
+    var title: String
+    var description: String
+    var isChecked: Bool = false
+}
+
 struct Todaychecklist: View {
+    @State private var checklistItems: [ChecklistItem] = [
+        ChecklistItem(title: "Task 1", description: "Description 1"),
+        ChecklistItem(title: "Task 2", description: "Description 2"),
+        ChecklistItem(title: "Task 3", description: "Description 3")
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(checklistItems.indices, id: \.self) { index in
+                HStack {
+                    Image(systemName: checklistItems[index].isChecked ? "checkmark.square.fill" : "square")
+                        .onTapGesture {
+                            checklistItems[index].isChecked.toggle()
+                        }
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(checklistItems[index].title)
+                            .font(.headline)
+                        
+                        Text(checklistItems[index].description)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+        }
+        .listStyle(PlainListStyle())
     }
 }
 
