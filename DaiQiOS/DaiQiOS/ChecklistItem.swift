b6e5439d0ad177223f34be346f248b5ededa2e5d
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-09-08 16:18:21 +0700

    update checklist

---
+++ DaiQiOS/DaiQiOS/ChecklistItem.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct ChecklistItem {
+    var title: String
+    var description: String
+    var isChecked: Bool = false
+}
--- DaiQiOS/DaiQiOS/Todaychecklist.swift
+++ DaiQiOS/DaiQiOS/Todaychecklist.swift
@@ -1,24 +1,6 @@
 import SwiftUI
 
-struct ChecklistItem {
-    var title: String
-    var description: String
-    var isChecked: Bool = false
-}
-
-struct Container:View{
-    var body: some View{
-        ZStack {
-            Color.white.edgesIgnoringSafeArea(.all)
-            
-            Todaychecklist()
-                .background(Color.white)
-                .padding()
-        }
-    }
-}
-
-struct Todaychecklist: View {
+struct TodayChecklist: View {
     @State private var checklistItems: [ChecklistItem] = [
         ChecklistItem(title: "Task 1", description: "Description 1"),
         ChecklistItem(title: "Task 2", description: "Description 2"),
@@ -26,51 +8,53 @@ struct Todaychecklist: View {
     ]
     
     var body: some View {
-        
-        VStack{
-            Text("Quest")
-                .padding(20)
-                .frame(alignment: .center)
-            
-            List {
-                Text("Today Checklist")
-                    .font(.title)
-                    .bold()
-                ForEach(checklistItems.indices, id: \.self) { index in
-                    HStack {
-                        Image(systemName: checklistItems[index].isChecked ? "checkmark.square.fill" : "square")
-                            .onTapGesture {
-                                checklistItems[index].isChecked.toggle()
-                            }
-                        
-                        VStack(alignment: .leading, spacing: 4) {
-                            Text(checklistItems[index].title)
-                                .font(.headline)
+        ZStack {
+            //            Color.white.edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Quest")
+                    .padding(20)
+                    .frame(alignment: .center)
+                
+                List {
+                    Text("Today Checklist")
+                        .font(.title)
+                        .bold()
+                    ForEach(checklistItems.indices, id: \.self) { index in
+                        HStack {
+                            Image(systemName: checklistItems[index].isChecked ? "checkmark.square.fill" : "square")
+                                .onTapGesture {
+                                    checklistItems[index].isChecked.toggle()
+                                }
                             
-                            Text(checklistItems[index].description)
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text(checklistItems[index].title)
+                                    .font(.headline)
+                                
+                                Text(checklistItems[index].description)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
                         }
                     }
                 }
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                        .font(.system(size: 24))
+                        .frame(width: 64, height: 64)
+                        .background(Color.purple.opacity(0.3))
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                }
             }
-            Button(action: {}) {
-                Image(systemName: "plus")
-                    .font(.system(size: 24))
-                    .frame(width: 64, height: 64)
-                    .background(Color.purple.opacity(0.3))
-                    .foregroundColor(.white)
-                    .clipShape(Circle())
-            }
+            
         }
         .listStyle(PlainListStyle())
-        
-        
-    }
-    
-    struct Todaychecklist_Previews: PreviewProvider {
-        static var previews: some View {
-            Container()
-        }
     }
 }
+
+struct TodayChecklist_Previews: PreviewProvider {
+    static var previews: some View {
+        TodayChecklist()
+    }
+}
+
