54ee3d91537dc999bbc7c7d1a80131faba9c816d
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-09-11 15:19:10 +0700

    create simple edittask view

--- DaiQiOS/DaiQiOS/EditTask.swift
+++ DaiQiOS/DaiQiOS/EditTask.swift
@@ -1,26 +1,44 @@
-//
-//  EditTask.swift
-//  DaiQiOS
-//
-//  Created by Đinh Trần Việt Anh on 11/09/2023.
-//
-
 import SwiftUI
 
-@State private var editTitle: String = ""
-
 struct EditTask: View {
+    @State private var title = ""
+    @State private var text = ""
+    
     var body: some View {
-                TextField(
-                "Title",
-                text: $editTitle
-            )
+        NavigationView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Title")
+                        .font(.headline)
+                        .padding(.trailing, 8)
+                    
+                    TextField("Enter title", text: $title)
+                        .padding()
+                        .border(Color.gray, width: 1)
+                        .cornerRadius(5)
+                }
+                
+                HStack {
+                    Text("Description")
+                        .font(.headline)
+                        .padding(.trailing, 8)
+                    
+                    TextEditor(text: $text)
+                        .frame(height: 200)
+                        .border(Color.gray, width: 1)
+                        .cornerRadius(5)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationBarTitle("Edit Task", displayMode: .inline)
         }
     }
-    
-    struct EditTask_Previews: PreviewProvider {
-        static var previews: some View {
-            EditTask()
-        }
+}
+
+struct EditTask_Previews: PreviewProvider {
+    static var previews: some View {
+        EditTask()
     }
 }
