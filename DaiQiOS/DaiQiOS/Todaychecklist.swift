29ed67392dbf14730b16daf6d15f09ca3e15d38f
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2023-09-11 11:21:44 +0700

    fix indentation

--- DaiQiOS/DaiQiOS/Todaychecklist.swift
+++ DaiQiOS/DaiQiOS/Todaychecklist.swift
@@ -1,36 +1,38 @@
 import SwiftUI
 struct TodayChecklist: View {
-var body: some View {
+    var body: some View {
         ZStack {
             VStack{
                 NavigationView {
-                   Text("Today Checklist")
-    }
-                .navigationTitle("Today Checklist")  .frame(height: 50)
+                    Text("Today Checklist")
+                }
+                .navigationTitle("Today Checklist")
+                .frame(height: 50)
                 .font(.title)
                 .bold()
-                            List {
-                  
-                 TaskCell()
-                    }     .listStyle(PlainListStyle())
+                
+                List {
+                    TaskCell()
                 }
-                addTaskButton()
+                .listStyle(PlainListStyle())
             }
-            
+            addTaskButton()
         }
-   
+        
     }
-    @ViewBuilder
-    private func addTaskButton() -> some View {
-        Button(action: {}) {
-            Image(systemName: "plus")
-                .font(.system(size: 24))
-                .frame(width: 64, height: 64)
-                .background(Color.purple.opacity(0.3))
-                .foregroundColor(.white)
-                .clipShape(Circle())
-        }
+    
+}
+@ViewBuilder
+private func addTaskButton() -> some View {
+    Button(action: {}) {
+        Image(systemName: "plus")
+            .font(.system(size: 24))
+            .frame(width: 64, height: 64)
+            .background(Color.purple.opacity(0.3))
+            .foregroundColor(.white)
+            .clipShape(Circle())
     }
+}
 
 
 struct TodayChecklist_Previews: PreviewProvider {
