886c1bc29741598a395a2fe28e310476c0f4ce6d
dinhtranvietanh <dinhtranvietanh253@gmail.com>
2023-09-21 16:00:07 +0700

    update button state addtask

--- DaiQiOS/DaiQiOS/AddTask.swift
+++ DaiQiOS/DaiQiOS/AddTask.swift
@@ -14,68 +14,79 @@ struct AddTask: View {
     var body: some View {
         NavigationView {
             VStack(alignment: .center) {
-                VStack(alignment: .leading) {
-                    TextField("Title", text: $title)
-                        .onChange(of: title) { newValue in
-                            if newValue.isEmpty {
-                                submitButtonDisabled = true
-                            } else {
-                                submitButtonDisabled = false
-                            }
-                        }
-                    Divider()
-                        .overlay(title.isEmpty ? .red : .gray)
-                    if title.isEmpty {
-                        Text("Title could not be empty")
-                            .foregroundColor(.red)
-                            .font(.caption)
-                            .multilineTextAlignment(.center)
-                    }
-                }
-                TextField("Description",text: $description, axis: .vertical)
-                    .lineLimit(1...5)
-                    .cornerRadius(5)
-                    .font(.body)
-                
-                Divider()
+                addTitle()
+                Spacer()
+                    .frame(height: 30)
+                addDescription()
                 Spacer()
                 submitAddTaskButton()
+                    .padding()
+                    .toolbar {
+                        ToolbarItem(placement: .principal) {
+                            Text("Add Task")
+                                .font(.title)
+                                .bold()
+                                .foregroundColor(.primary)
+                        }
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button("Cancel") {
+                                presentationMode.wrappedValue.dismiss()
+                            }
+                        }
+                    }
             }
-            .padding()
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text("Add Task")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.primary)
-                }
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button("Cancel") {
-                        presentationMode.wrappedValue.dismiss()
+        }
+    }
+    func addTitle() -> some View {
+        VStack(alignment: .leading) {
+            TextField("Title", text: $title)
+                .onChange(of: title) { newValue in
+                    if newValue.isEmpty {
+                        submitButtonDisabled = true
+                    } else {
+                        submitButtonDisabled = false
                     }
                 }
+            Divider()
+                .overlay(title.isEmpty ? .red : .gray)
+            if title.isEmpty {
+                Text("Title could not be empty")
+                    .foregroundColor(.red)
+                    .font(.caption)
+                    .multilineTextAlignment(.center)
+                    .transition(AnyTransition.opacity.animation(.linear(duration: 0.5)))
             }
         }
     }
-    @ViewBuilder
-    private func submitAddTaskButton() -> some View {
-        Button(action: {
-            saveChanges()
-        }) {
-            Text("Submit")
-                .font(.headline)
-                .padding()
-        }
-        .tint(.purple.opacity(0.3))
-        .controlSize(.small) 
-        .buttonStyle(.borderedProminent)
-        .disabled(submitButtonDisabled)
+
+func addDescription() -> some View {
+    VStack {
+        TextField("Description",text: $description, axis: .vertical)
+            .lineLimit(1...5)
+            .cornerRadius(5)
+            .font(.body)
+        Divider()
     }
-    @State private var submitButtonDisabled = true
-    private func saveChanges() {
-        let checklistItem = ChecklistItem(title: title, description: description)
-        onTaskSubmit?(checklistItem)
-        presentationMode.wrappedValue.dismiss()
+}
+@ViewBuilder
+private func submitAddTaskButton() -> some View {
+    Button(action: {
+        saveChanges()
+    }) {
+        Text("Submit")
+            .font(.headline)
+            .padding()
     }
+    .tint(.purple.opacity(0.3))
+    .controlSize(.small)
+    .buttonStyle(.borderedProminent)
+    .disabled(submitButtonDisabled)
+}
+@State private var submitButtonDisabled = true
+private func saveChanges() {
+    let checklistItem = ChecklistItem(title: title, description: description)
+    onTaskSubmit?(checklistItem)
+    presentationMode.wrappedValue.dismiss()
+}
 }
 
--- DaiQiOS/DaiQiOS/EditTask.swift
+++ DaiQiOS/DaiQiOS/EditTask.swift
@@ -8,30 +8,10 @@ struct EditTask: View {
     var body: some View {
         NavigationView {
             VStack(alignment: .center) {
-                VStack(alignment: .leading) {
-                    TextField("Title", text: $title)
-                        .onChange(of: title) { newValue in
-                            if newValue.isEmpty {
-                                submitButtonDisabled = true
-                            } else {
-                                submitButtonDisabled = false
-                            }
-                        }
-                    Divider()
-                        .overlay(title.isEmpty ? .red : .gray)
-                    if title.isEmpty {
-                        Text("Title could not be empty")
-                            .foregroundColor(.red)
-                            .font(.caption)
-                            .multilineTextAlignment(.center)
-                    }
-                }
-                TextField("Description",text: $description, axis: .vertical)
-                    .lineLimit(1...5)
-                    .cornerRadius(5)
-                    .font(.body)
-                
-                Divider()
+                editTitle()
+                Spacer()
+                    .frame(height: 30)
+                editDescription()
                 Spacer()
                 submitEditButton()
             }
@@ -44,6 +24,37 @@ struct EditTask: View {
         .navigationTitle("Edit Task")
     }
     
+    func editTitle() -> some View {
+        VStack(alignment: .leading) {
+            TextField("Title", text: $title)
+                .onChange(of: title) { newValue in
+                    if newValue.isEmpty {
+                        submitButtonDisabled = true
+                    } else {
+                        submitButtonDisabled = false
+                    }
+                }
+            Divider()
+                .overlay(title.isEmpty ? .red : .gray)
+            if title.isEmpty {
+                Text("Title could not be empty")
+                    .foregroundColor(.red)
+                    .font(.caption)
+                    .multilineTextAlignment(.center)
+                    .transition(AnyTransition.opacity.animation(.linear(duration: 0.5)))
+            }
+        }
+    }
+    
+    func editDescription () -> some View {
+        VStack {
+            TextField("Description",text: $description, axis: .vertical)
+                .lineLimit(1...5)
+                .cornerRadius(5)
+                .font(.body)
+            Divider()
+        }
+    }
     @ViewBuilder
     private func submitEditButton() -> some View {
         Button(action: {
