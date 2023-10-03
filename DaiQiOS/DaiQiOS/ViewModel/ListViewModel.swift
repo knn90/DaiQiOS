6bbfa660b2483f982ebf019a1f2e550362d80b50
dinhtranvietanh <dinhtranvietanh253@gmail.com>
2023-10-03 14:24:26 +0700

    try reset listitem daily 1

--- DaiQiOS/DaiQiOS/ViewModel/ListViewModel.swift
+++ DaiQiOS/DaiQiOS/ViewModel/ListViewModel.swift
@@ -10,9 +10,10 @@ class ListViewModel: ObservableObject {
     let itemsKey : String = "items_list"
     init() {
         getItems()
+        resetIfNewDay()
     }
     
-    func getItems() { 
+    func getItems() {
         guard
             let data = UserDefaults.standard.data(forKey: itemsKey),
             let savedItems = try? JSONDecoder().decode([ChecklistItem].self, from: data)
@@ -24,12 +25,25 @@ class ListViewModel: ObservableObject {
             checklistItems.remove(at: index)
         }
     }
-      func move(indices: IndexSet, newOffset: Int) {
+    func move(indices: IndexSet, newOffset: Int) {
         checklistItems.move(fromOffsets: indices, toOffset: newOffset)
     }
     func saveItems() {
         if let encodedData = try? JSONEncoder().encode(checklistItems) {
             UserDefaults.standard.set(encodedData, forKey: itemsKey)
+        }
+    }
+    func resetIfNewDay() {
+        let lastResetDate = UserDefaults.standard.value(forKey: "lastResetDate") as? Date
+        let currentDate = Date()
+        
+        let calendar = Calendar.current
+        let lastResetDateComponents = calendar.dateComponents([.day], from: lastResetDate ?? Date())
+        let currentDateComponents = calendar.dateComponents([.day], from: currentDate)
+        
+        if lastResetDateComponents.day != currentDateComponents.day {
+            UserDefaults.standard.set(currentDate, forKey: "lastResetDate")
+            checklistItems = []
         }
     }
 }
