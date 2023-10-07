95ae887051a6364a3ba3c23b9c7d65d080a5528d
dinhtranvietanh <dinhtranvietanh253@gmail.com>
2023-10-07 14:14:10 +0700

    connect firebase

--- DaiQiOS/DaiQiOS/DaiQiOSApp.swift
+++ DaiQiOS/DaiQiOS/DaiQiOSApp.swift
@@ -1,7 +1,10 @@
 import SwiftUI
+import Firebase
 
 @main
 struct DaiQiOSApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     @StateObject var listViewModel: ListViewModel = ListViewModel()
     @StateObject var addTaskViewModel: AddTaskViewModel = AddTaskViewModel()
     @StateObject var editTaskViewModel: EditTaskViewModel = EditTaskViewModel()
@@ -11,7 +14,7 @@ struct DaiQiOSApp: App {
             NavigationView {
                 
                 TodayChecklist()
-                    
+                
             }
             .environmentObject(listViewModel)
             .environmentObject(addTaskViewModel)
@@ -19,5 +22,11 @@ struct DaiQiOSApp: App {
             
         }
         
+    }
+}
+class AppDelegate : NSObject , UIApplicationDelegate {
+    func application ( _ application : UIApplication , didFinishLaunchingWithOptions launchOptions : [ UIApplication . LaunchOptionsKey : Any ]? = nil ) -> Bool {
+        FirebaseApp . configure ()
+        return true
     }
 }
